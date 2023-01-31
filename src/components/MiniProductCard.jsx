4b1a35037bc3dc5a98facd00ce8b1f58dda0f15d
nelly476 <nellybulatova12@gmail.com>
2023-01-31 13:54:40 +0300

    add buttons to cart

--- src/components/MiniProductCard.jsx
+++ src/components/MiniProductCard.jsx
@@ -1,10 +1,25 @@
+import { useState } from "react";
+
 export default function MiniProductCard(props) {
   const { id, name, price, img } = props.item;
+  const [amount, setAmount] = useState(1);
+
+  function changeCount(num) {
+    setAmount((prev) => prev + num);
+  }
+
   return (
-    <span className="product--card">
-      <p>{name}</p>
-      <p>{price}</p>
+    <span className="mini--product--card">
       <img src={img} alt="" />
+      <div className="mini--name--price">
+        <h3>{name}</h3>
+        <p>{price}</p>
+        <span className="add--remove--section">
+          <i className="fa-solid fa-minus" onClick={() => changeCount(-1)}></i>
+          <p>{amount}</p>
+          <i className="fa-solid fa-plus" onClick={() => changeCount(1)}></i>
+        </span>
+      </div>
     </span>
   );
 }
