9b083d81df522655fcd326e29dc8382f1eb7be94
nelly476 <nellybulatova12@gmail.com>
2023-02-04 21:54:36 +0300

    add Contact page

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,3 +1,23 @@
-export default function Products() {
-  return <div></div>;
+export default function Contact() {
+  return (
+    <div className="contact--container">
+      <a
+        href="https://www.linkedin.com/in/nelly-bulatova-3211211a8"
+        className="social--link"
+      >
+        <i className="fa-brands fa-linkedin"></i>
+      </a>
+
+      <a href="https://github.com/nelly476" className="social--link">
+        <i className="fa-brands fa-github"></i>
+      </a>
+
+      <a
+        href="https://instagram.com/sadnsorryy?igshid=YmMyMTA2M2Y="
+        className="social--link"
+      >
+        <i className="fa-brands fa-instagram"></i>
+      </a>
+    </div>
+  );
 }
